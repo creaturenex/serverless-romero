49b0f42d50f10bbfc7cbaa29fe891d2a7d79ee74
Oscar Romero <creaturenex@gmail.com>
2021-07-01 17:56:44 -0400

    Complete Step 3

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,23 +1,47 @@
-var multipart = require('parse-multipart');
+const fetch = require('node-fetch');
+const multipart = require('parse-multipart');
+
+async function analyzeImage(img) {
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'
+    })
+
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',
+        body: img,
+        headers: {
+            'Content-Type': "application/octet-stream",
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    })
+    let emotionData = await resp.json();
+
+    return emotionData;
+}
 
 module.exports = async function (context, req) {
 
-    var boundary = multipart.getBoundary(req.headers['content-type']);
-    console.log(boundary)
+    let boundary = multipart.getBoundary(req.headers['content-type']);
 
-    var parts = multipart.Parse(req.body, boundary);
-    console.log(parts)
+    // req.body == body
+    let parts = multipart.Parse(req.body, boundary);
 
-    var convertedResult = Buffer.from(parts[0].data).toString('base64');
-    console.log(convertedResult)
+    let imagedata = Buffer.from(parts[0].data);
 
-    const responseMessage = (convertedResult)
-        ? convertedResult
-        : "Please post an image in the body"
+    let result = await analyzeImage(imagedata);
 
-        context.res = {
-        // status: 200, /* Defaults to 200 */
+    const responseMessage = (result) ?
+        { result } :
+        "Please post an image in the body";
+
+    context.res = {
         body: responseMessage
     };
+
+    console.log(result)
     context.done();
 }
