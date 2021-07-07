8b2f3ff50db88f2b0138a366ab26bafa3a7f7544
Oscar Romero <creaturenex@gmail.com>
2021-07-07 15:43:41 -0400

    workaround to querystring

--- deepsecrets/index.js
+++ deepsecrets/index.js
@@ -1,12 +1,16 @@
-const querystring = require('querystring');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    context.log(req.body)
 
-    const queryObject = querystring.parse(req.body);
+    let parseBody = new URL ("https://test.com/test?" + req.body.toString())
+    context.log(parseBody)
+
+    let result = parseBody.searchParams.get('Body')
+    context.log(result)
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: queryObject.Body
+        body: result
     };
 }
