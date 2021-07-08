9e31ded9cf26b2771d96aeee755c4cef10b68af9
Oscar Romero <creaturenex@gmail.com>
2021-07-07 21:09:30 -0400

    Added cosmo database function

--- deepsecrets/index.js
+++ deepsecrets/index.js
@@ -1,16 +1,68 @@
+const CosmosClient = require("@azure/cosmos").CosmosClient;
+
+const config = {
+    endpoint: process.env.COSMO_ENDPOINT,
+    key: process.env.COSMO_KEY,
+    databaseId: "SecretStorer",
+    containerId: "secrets",
+    partitionKey: {
+        kind: "Hash",
+        paths: ["/secrets"]
+    }
+};
+
+async function create(client) {
+    const {
+        database
+    } = await client.databases.createIfNotExists({
+        id: config.databaseId
+    });
+
+    const {
+        container
+    } = await client
+        .database(config.databaseId)
+        .containers.createIfNotExists({
+            id: config.containerId,
+            key: config.partitionKey
+        }, {
+            offerThroughput: 400
+        });
+
+}
+
+async function createDocument(newItem) {
+    var { endpoint, key, databaseId, containerId } = config;
+    const client = new CosmosClient({endpoint, key});
+    const database = client.database(databaseId);
+    const container = database.container(containerId);
+    await create(client, databaseId, containerId);
+
+    const querySpec = {query: "SELECT top 1 * FROM c order by c._ts desc"};
+
+    const { resources: items } = await container.items.query(querySpec).fetchAll();
+
+    const {resource: createdItem} = await container.items.create(newItem);
+
+    return items
+}
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    context.log(req.body)
 
-    let parseBody = new URL ("https://test.com/test?" + req.body.toString())
-    context.log(parseBody)
+    let parseBody = new URL("https://test.com/test?" + req.body.toString())
+    console.log(parseBody)
+    let message = parseBody.searchParams.get('Body')
+    context.log(message)
+    let document = { "message" : message }
 
-    let result = parseBody.searchParams.get('Body')
-    context.log(result)
+    let items = await createDocument(document);
+    context.log(items)
+
+    const responseMessage = `Thanks 😊! Your secret: "${message} was sent to the Cosmos!". 😯 Someone confessed that: ${JSON.stringify(items[0].message)}`
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: result
+        body: responseMessage
     };
 }
