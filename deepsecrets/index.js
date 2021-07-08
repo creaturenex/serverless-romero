4f5338f0a2d69e3b10b21fe0b270f23d411ec5bf
Oscar Romero <creaturenex@gmail.com>
2021-07-07 22:12:14 -0400

    updated to match format in step 8

--- deepsecrets/index.js
+++ deepsecrets/index.js
@@ -32,8 +32,16 @@ async function create(client) {
 }
 
 async function createDocument(newItem) {
-    var { endpoint, key, databaseId, containerId } = config;
-    const client = new CosmosClient({endpoint, key});
+    var {
+        endpoint,
+        key,
+        databaseId,
+        containerId
+    } = config;
+    const client = new CosmosClient({
+        endpoint,
+        key
+    });
     const database = client.database(databaseId);
     const container = database.container(containerId);
     await create(client, databaseId, containerId);
@@ -42,9 +50,13 @@ async function createDocument(newItem) {
         query: "SELECT * from c"
     };
 
-    const { resources: items } = await container.items.query(querySpec).fetchAll();
+    const {
+        resources: items
+    } = await container.items.query(querySpec).fetchAll();
 
-    const {resource: createdItem} = await container.items.create(newItem);
+    const {
+        resource: createdItem
+    } = await container.items.create(newItem);
 
     return items
 }
@@ -53,17 +65,21 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     let parseBody = new URL("https://test.com/test?" + req.body.toString())
-    console.log(parseBody)
+
     let message = parseBody.searchParams.get('Body')
-    context.log(message)
-    let document = { "message" : message }
+
+    let document = {
+        "message": message
+    }
 
     let items = await createDocument(document);
     context.log(items)
 
     var random_value = Math.floor(items.length * Math.random());
 
-    const responseMessage = `Thanks 😊! Stored your secret "${message}". 😯 Someone confessed that: ${JSON.stringify(items[random_value].message)}`
+    let randomSecret = items[random_value].message
+
+    const responseMessage = `Thanks 😊! Stored your secret "${message}". 😯 Someone confessed that: ${JSON.stringify(randomSecret)}`
 
     context.res = {
         // status: 200, /* Defaults to 200 */
