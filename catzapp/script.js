2c00a2fb9d812e52761b2b9824a5cba06a332c1b
Oscar Romero <creaturenex@gmail.com>
2021-07-19 02:50:36 +0200

    updated cat app to return 4 images

--- catzapp/script.js
+++ catzapp/script.js
@@ -3,11 +3,27 @@ async function y1k3s() {
 
   let url = "https://cataas.com/cat/says/"
 
-  let name = document.getElementById('username').value
+  let name1 = document.getElementById('name1').value
 
-  if (name != '') {
-    document.getElementById('image').src = `${url}` + `${name}`
+  if (name1 != '') {
+    document.getElementById('image1').src = `${url}` + `${name1}`
+  }
 
-    console.log("Getting your image...");
+  let name2 = document.getElementById('name2').value
+
+  if (name2 != '') {
+    document.getElementById('image2').src = `${url}` + `${name2}`
+  }
+
+  let name3 = document.getElementById('name3').value
+
+  if (name3 != '') {
+    document.getElementById('image3').src = `${url}` + `${name3}`
+  }
+
+  let name4 = document.getElementById('name4').value
+
+  if (name4 != '') {
+    document.getElementById('image4').src = `${url}` + `${name4}`
   }
 }
