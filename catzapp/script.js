ba002d39f83ba188e885b6555c0031c564f20552
Oscar Romero <creaturenex@gmail.com>
2021-07-19 03:03:11 +0200

    updated script to convert base64

--- catzapp/script.js
+++ catzapp/script.js
@@ -6,24 +6,24 @@ async function y1k3s() {
   let name1 = document.getElementById('name1').value
 
   if (name1 != '') {
-    document.getElementById('image1').src = `${url}` + `${name1}`
+    document.getElementById('image1').src = "data:image/png;base64, " + `${url}` + `${name1}`
   }
 
   let name2 = document.getElementById('name2').value
 
   if (name2 != '') {
-    document.getElementById('image2').src = `${url}` + `${name2}`
+    document.getElementById('image2').src = "data:image/png;base64, " + `${url}` + `${name2}`
   }
 
   let name3 = document.getElementById('name3').value
 
   if (name3 != '') {
-    document.getElementById('image3').src = `${url}` + `${name3}`
+    document.getElementById('image3').src = "data:image/png;base64, " + `${url}` + `${name3}`
   }
 
   let name4 = document.getElementById('name4').value
 
   if (name4 != '') {
-    document.getElementById('image4').src = `${url}` + `${name4}`
+    document.getElementById('image4').src = "data:image/png;base64, " + `${url}` + `${name4}`
   }
 }
