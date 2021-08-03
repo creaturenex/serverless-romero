181dbb457758e02e212f3a74adb7a81deea79993
Oscar Romero <creaturenex@gmail.com>
2021-08-03 15:49:23 -0400

    current version of app, also added upload/timer function but it is not need atm

--- magicvisiontool/index.js
+++ magicvisiontool/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const querystring = require('querystring'); //twilio
 const async = require('async');
 const fs = require('fs');
 const https = require('https');
@@ -9,82 +10,90 @@ const sleep = require('util').promisify(setTimeout);
 const ComputerVisionClient = require('@azure/cognitiveservices-computervision').ComputerVisionClient;
 const ApiKeyCredentials = require('@azure/ms-rest-js').ApiKeyCredentials;
 
-/**
- * AUTHENTICATE
- * This single client is used for all examples.
- */
-const key = process.env.MAGICVISIONAIKEY;
-const endpoint = process.env.MAGICVISIONAIENDPOINT;
+module.exports = async function (context, req) {
+    const key = process.env.MAGICVISIONAIKEY;
+    const endpoint = process.env.MAGICVISIONAIENDPOINT;
 
-const computerVisionClient = new ComputerVisionClient(
-    new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } }), endpoint);
+    const computerVisionClient = new ComputerVisionClient(new ApiKeyCredentials({
+        inHeader: {
+            'Ocp-Apim-Subscription-Key': key
+        }
+    }), endpoint);
 
-function computerVision() {
-    async.series([
-        async function () {
+    function computerVision(imageUrl) {
+        async.series([
+            async function () {
 
-                // URL images containing printed and/or handwritten text.
-                // The URL can point to image files (.jpg/.png/.bmp) or multi-page files (.pdf, .tiff).
-                const printedTextSampleURL = 'https://c1.scryfall.com/file/scryfall-cards/large/front/6/7/67a76010-d932-4727-8b5e-b8e2d14e1362.jpg?1624997941';
+                    const STATUS_SUCCEEDED = "succeeded";
+                    const STATUS_FAILED = "failed"
 
+                    // Recognize text in printed image from a URL
+                    context.log('Read printed text from URL...', imageUrl.split('/').pop());
+                    const printedResult = await readTextFromURL(computerVisionClient, imageUrl);
+                    printRecText(printedResult);
 
-                // Status strings returned from Read API. NOTE: CASING IS SIGNIFICANT.
-                // Before Read 3.0, these are "Succeeded" and "Failed"
-                const STATUS_SUCCEEDED = "succeeded";
-                const STATUS_FAILED = "failed"
+                    // Perform read and await the result from URL
+                    async function readTextFromURL(client, url) {
+                        // To recognize text in a local image, replace client.read() with readTextInStream() as shown:
+                        let result = await client.read(url);
+                        context.log(result)
 
+                        // Operation ID is last path segment of operationLocation (a URL)
+                        let operation = result.operationLocation.split('/').slice(-1)[0];
+                        context.log(operation)
 
-                // Recognize text in printed image from a URL
-                console.log('Read printed text from URL...', printedTextSampleURL.split('/').pop());
-                const printedResult = await readTextFromURL(computerVisionClient, printedTextSampleURL);
-                printRecText(printedResult);
-
-                // Perform read and await the result from URL
-                async function readTextFromURL(client, url) {
-                    // To recognize text in a local image, replace client.read() with readTextInStream() as shown:
-                    let result = await client.read(url);
-                    console.log(result)
-
-                    // Operation ID is last path segment of operationLocation (a URL)
-                    let operation = result.operationLocation.split('/').slice(-1)[0];
-                    console.log(operation)
-
-                    // Wait for read recognition to complete
-                    // result.status is initially undefined, since it's the result of read
-                    while (result.status !== STATUS_SUCCEEDED) {
-                        await sleep(1000);
-                        result = await client.getReadResult(operation);
-                    }
-                    return result.analyzeResult.readResults; // Return the first page of result. Replace [0] with the desired page if this is a multi-page file such as .pdf or .tiff.
-                }
-
-                // Prints all text from Read result
-                function printRecText(readResults) {
-                    console.log('Recognized text:');
-                    for (const page in readResults) {
-                        if (readResults.length > 1) {
-                            console.log(`==== Page: ${page}`);
+                        // Wait for read recognition to complete
+                        // result.status is initially undefined, since it's the result of read
+                        while (result.status !== STATUS_SUCCEEDED) {
+                            await sleep(1000);
+                            result = await client.getReadResult(operation);
                         }
-                        const result = readResults[page];
-                        if (result.lines.length) {
-                            for (const line of result.lines) {
-                                console.log(line.words.map(w => w.text).join(' '));
+                        return result.analyzeResult.readResults;
+                        // Return the first page of result.
+                    }
+
+                    // Prints all text from Read result
+                    function printRecText(readResults) {
+
+                        context.log('Recognized text:');
+                        for (const page in readResults) {
+                            if (readResults.length > 1) {
+                                context.log(`==== Page: ${page}`);
                             }
-                        } else {
-                            console.log('No recognized text.');
+                            const result = readResults[page];
+                            if (result.lines.length) {
+                                context.log('<<<' + result.lines[0].text + '>>>') //<<<<<<<<<<<<< I want this!!!!(result.lines[0].text) return this so I can access it and pass it as an argument to Scryfall with a simple fetch function
+                                for (const line of result.lines) {
+                                    context.log(line.words.map(w => w.text).join(' '));
+                                }
+                            } else {
+                                context.log('No recognized text.');
+                            }
                         }
                     }
+
+                },
+                function () {
+                    return new Promise((resolve) => {
+                        resolve();
+                    })
                 }
+        ], (err) => {
+            throw (err);
+        });
+    }
 
-            },
-            function () {
-                return new Promise((resolve) => {
-                    resolve();
-                })
-            }
-    ], (err) => {
-        throw (err);
-    });
+
+    // twilio section
+
+    context.log('JavaScript HTTP trigger function processed a request.');
+    const queryObject = querystring.parse(req.body);
+    let cardUrl = queryObject.MediaUrl0
+
+    computerVision(cardUrl) //scoping issues we think! how to access result in nested function
+
+    context.res = {
+        body: 'Image scanned' //responseMessage
+    };
+    context.done();
 }
-
-computerVision();
